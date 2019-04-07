c83440923e0f9957e6ef288eb4f9953b6b3fd272
Samuel Baptista <samuel.baptista@ecodev.ch>
2019-04-07 22:39:18 +0200

    Allow custom placeholder for empty search field

--- projects/natural-search/src/lib/search/search.component.ts
+++ projects/natural-search/src/lib/search/search.component.ts
@@ -1,8 +1,8 @@
 import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChildren } from '@angular/core';
+import { deepClone } from '../classes/utils';
 import { NaturalInputComponent } from '../input/input.component';
 import { NaturalSearchConfiguration } from '../types/Configuration';
 import { NaturalSearchSelections } from '../types/Values';
-import { deepClone } from '../classes/utils';
 
 @Component({
     selector: 'natural-search',
@@ -13,6 +13,7 @@ export class NaturalSearchComponent implements OnChanges {
 
     @ViewChildren(NaturalInputComponent) inputs: NaturalInputComponent[];
 
+    @Input() placeholder = 'Rechercher';
     @Input() configurations: NaturalSearchConfiguration;
     @Input() multipleGroups: false;
 
